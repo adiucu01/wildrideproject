0c54edb531918645eff9a0ec6bb7da9757561911
Cristian CHIRIC <cristian.chiric@gmail.com>
2013-06-06 13:18:42 +0300

    Missing files

---
+++ application/admin/models/ModelAdmin.php
@@ -0,0 +1,51 @@
+<?php
+/*
+  require_once("/../../../classes/BD.php");
+  require_once("/../../../config/config.php");
+*/    
+  class ModelAdmin{
+      private static $db;
+      public function __construct(){
+          self::$db = new DB(); 
+      }
+      public function addAdmin($param){ 
+           $password = $this->cryptp($param['password']);
+           $sql = "INSERT INTO admin(nume, prenume, email, parola, id_punct_de_lucru, tip_admin)
+                    VALUES('{$param['first_name']}','{$param['last_name']}','{$param['email']}','{$password}',{$param['punct_lucru']},2)";   
+           
+           $result = self::$db->query($sql);  
+           
+           return $result;  
+      }
+      public function updateUser($param){
+           $sql = "UPDATE admin
+                    SET nume='{$param['first_name']}', prenume='{$param['last_name']}', email='{$param['email']}', id_punct_de_lucru={$param['punct_lucru']}, tip_admin={$param['tip_utilizator']}
+                    WHERE id={$param['id']}";   
+           
+           $result = self::$db->query($sql);  
+           
+           return $result;
+      }
+      public function deleteUser($id){
+          $sql = "DELETE FROM admin
+                    WHERE id = {$id}";   
+           
+           $result = self::$db->query($sql);  
+           
+           return $result;
+      }
+      public function deleteUsers($param){
+          foreach($param['select'] as $item){
+              $sql = "DELETE FROM admin
+                    WHERE id = {$item}";   
+           
+              $result = self::$db->query($sql); 
+          }           
+           
+           return $result;
+      }
+      private function cryptp($password){ 
+        return  base64_encode(md5($password.PRIVATE_KEY.$password));
+      }
+  }
+?>
